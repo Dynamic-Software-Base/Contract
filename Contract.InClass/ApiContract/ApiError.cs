8bd900ec84274ae710e17e6c3c55eabae4a44cca
EmperorXSama <5alidghoddane@protonmail.com>
2026-03-06 12:22:46 +0000

    update name

--- Contract.InClass/ApiContract/ApiError.cs
+++ Contract.InClass/ApiContract/ApiError.cs
@@ -4,7 +4,7 @@ public sealed class ApiError
 {
     public string Code { get; init; } = string.Empty;
     public string Message { get; init; } = string.Empty;
-    public ApiError Type{ get; init; }
+    public ApiErrorType Type{ get; init; }
     
     public string? PropertyName { get; init; }
 }
