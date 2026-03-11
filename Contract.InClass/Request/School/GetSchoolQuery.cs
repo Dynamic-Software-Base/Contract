c5b6b3369532c45719d5aa85fcd03d9785ba9c57
EmperorXSama <5alidghoddane@protonmail.com>
2026-03-11 12:34:38 +0000

    add new release 1.0.11 and add GetSchoolQuery.cs

---
+++ Contract.InClass/Request/School/GetSchoolQuery.cs
@@ -0,0 +1,5 @@
+﻿using Contract.InClass.Common;
+
+namespace Contract.InClass.Request.School;
+
+public record GetSchoolQuery(string? NameSearch,string? City,GradeLevelOfferingDto? GradeLevelOffering);
