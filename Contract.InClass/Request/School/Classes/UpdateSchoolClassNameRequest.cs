4e5924a141b6dccf0fb9568014aa82aae8be8915
EmperorXSama <5alidghoddane@protonmail.com>
2026-04-02 12:04:22 +0100

    update name and capcaity request object added

---
+++ Contract.InClass/Request/School/Classes/UpdateSchoolClassNameRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Contract.InClass.Request.School.Classes;
+public record UpdateSchoolClassNameRequest(
+    Guid SchoolClassId,
+    string Name);
+
+public record UpdateSchoolClassCapacityRequest(
+    Guid SchoolClassId,
+    int MaxStudents);
