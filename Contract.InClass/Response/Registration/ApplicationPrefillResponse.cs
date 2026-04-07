5b40a5d3203070a8cc910a844544b59c9e20ff1d
EmperorXSama <5alidghoddane@protonmail.com>
2026-04-07 11:18:04 +0100

    ApplicationPrefillResponse

---
+++ Contract.InClass/Response/Registration/ApplicationPrefillResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Contract.InClass.Response.Registration;
+
+public sealed record ApplicationPrefillResponse(
+    string StudentFirstName,
+    string StudentLastName,
+    string ContactPhone,
+    string? ContactEmail,
+    Dictionary<string, object?> PreFilledFormValues,
+    int MatchedFieldCount,
+    int TotalFieldCount
+);
